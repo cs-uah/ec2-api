d1bd8a721ded55316a369d2adf1eb23cc2be4606
CS-UAH <support@cs.uah.edu>
2019-05-02 22:34:08 -0500

    Added line to make role create

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,7 @@
 openstack endpoint create --region RegionOne ec2api public http://controller:8788/
 openstack endpoint create --region RegionOne ec2api admin http://controller:8788/
 openstack endpoint create --region RegionOne ec2api internal http://controller:8788/
+openstack role create service
 read -sp "Enter the password for the openstack ec2api account (EC2_PASS):" EC2_PASS
 echo ""
 openstack user create --domain default --password $ec2_pass ec2api
